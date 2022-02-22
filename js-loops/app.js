1ac8cfadd3d2888bfedef1052271c83f1f782888
Patrick Brennan <patbrennan7@gmail.com>
2022-02-22 16:15:30 -0500

    Loops Continued

--- js-loops/app.js
+++ js-loops/app.js
@@ -1,4 +1,3 @@
-
 //* For Loops!
 
 //! This is the consistent Syntax and structure of a FOR LOOP
@@ -12,28 +11,28 @@
 //? i starts at 1, as long as it's less than or equal to 10,
 //? it will increment i by 1, and LOOP again
 for (let i = 1; i <= 10; i++) {
-    console.log(i);
+	console.log(i);
 }
 
 //? for loop that prints EVEN numbers
-for (let i = 0; i <= 20; i += 2) { 
-    console.log(i);
+for (let i = 0; i <= 20; i += 2) {
+	console.log(i);
 }
 
 //? for loop that prints ODD numbers
-for (let i = 1; i <= 21; i += 2) { 
-    console.log(i);
+for (let i = 1; i <= 21; i += 2) {
+	console.log(i);
 }
 
 //? for loop that counts down from 100
 for (let i = 100; i >= 0; i -= 10) {
-    console.log(i);
+	console.log(i);
 }
 
 //? getting weird with it
 //? i starts at 10, and as long as it's less than or equal to 1000, i * 10
 for (let i = 10; i <= 1000; i *= 10) {
-    console.log(i);
+	console.log(i);
 }
 
 //* Infinite loops!
@@ -47,53 +46,91 @@ for (let i = 10; i <= 1000; i *= 10) {
 //* Looping over Arrays
 
 const animalBeers = [
-    'Lions Head',
-    'Dogfish Head',
-    'Dirt Wolf',
-    'Golden Monkey',
-    'Grapefruit Sculpin',
-    'Raging Eagle',
+	"Lions Head",
+	"Dogfish Head",
+	"Dirt Wolf",
+	"Golden Monkey",
+	"Grapefruit Sculpin",
+	"Raging Eagle",
 ];
 
 //? this will loop over each INDEX of the animalBeers array
 //? could also be written as "i <= animalBeers.length - 1"
 //? if left at "i <= animalBeers.length" you'll get an "undefined" at the end
-for (let i = 0; i < animalBeers.length; i++){
-    console.log(i, animalBeers[i]); //? i prints the index, animalBeers[i] prints the value
+for (let i = 0; i < animalBeers.length; i++) {
+	console.log(i, animalBeers[i]); //? i prints the index, animalBeers[i] prints the value
 }
 
 //? reverse it!
 
 for (let i = animalBeers.length - 1; i >= 0; i--) {
-    console.log(i, animalBeers[i]);
+	console.log(i, animalBeers[i]);
 }
 
 //* Nested For Loops!
 
-
 //? i will run one cycle
 for (let i = 1; i <= 10; i++) {
-    console.log(`i is: ${i}`);
-    //? and then j will run all of it's cycles
-    for (let j = 1; j < 4; j++) { 
-        console.log(`     j is: ${i}`)
-    }
+	console.log(`i is: ${i}`);
+	//? and then j will run all of it's cycles
+	for (let j = 1; j < 4; j++) {
+		console.log(`     j is: ${i}`);
+	}
 }
 
 //* using a nested for loop to iterate through a class of students
 const seatingChart = [
-    ['Person', 'mitchell', 'scraggy'],
-    ['john', 'jimmy', 'juggle'],
-    ['jimbo', 'wiggle', 'cauliflower']
-]
+	["Person", "mitchell", "scraggy"],
+	["john", "jimmy", "juggle"],
+	["jimbo", "wiggle", "cauliflower"],
+];
 
 //? the first loop will grab the first array (the row of students)
 for (let i = 0; i < seatingChart.length; i++) {
-    const row = seatingChart[i];
-    console.log(`ROW ${i + 1}`);
-    //? The second loop will then go through everything in that array (the students themselves)
-    for (let j = 0; j < row.length; j++) {
-        console.log(row[j]);
-    }
-    //? and then do it all again!
+	const row = seatingChart[i];
+	console.log(`ROW ${i + 1}`);
+	//? The second loop will then go through everything in that array (the students themselves)
+	for (let j = 0; j < row.length; j++) {
+		console.log(row[j]);
+	}
+	//? and then do it all again!
 }
+
+//* While Loops
+//? Very similiar to FOR loops, just looks a little cleaner
+//? and the logic is a little more straight forward
+
+//? WHILE count is less than 10, *Execute code inside curly braces*
+let count = 0;
+while (count < 10) {
+	count++;
+	console.log(count);
+}
+
+//! WARNING Very easy to make infinite loops!
+
+//? Good for things that you don't know how many times you iterate through something
+//? Like games!
+
+// while (!gameOver) {
+// 	//player 1 turn
+// 	//player 2 turn
+// }
+// Congrats you've exited the loop and won the game!
+
+// const SECRET = "BIGHIPPO";
+
+// let guess = window.prompt("guess the secret!");
+// while (guess !== SECRET) {
+// 	guess;
+// }
+// console.log("Congrats it's  a BIG HIPPO!!!");
+
+//* The Break Keyword
+
+// let input = prompt("Say something!");
+// while (true) {
+// 	window.prompt(input);
+// 	if (input.toLowerCase() === "stop copying me") break;
+// }
+// console.log("Okay Jeez!");
