12c7fee188291887030095a366b82ae3a1ed4f79
Patrick Brennan <patbrennan7@gmail.com>
2022-03-04 16:28:35 -0500

    Multiple Ways, Same Thing

--- js-arrow-functions/app.js
+++ js-arrow-functions/app.js
@@ -25,3 +25,110 @@ function sum3(x, y) {
 
 //? particularly useful for functions that are only
 //? used when passed to other functions, like with .map()
+
+//! Arrow functions CANNOT be written on there own!
+//! They MUST be saved to a variable when standing alone
+
+(x, y) => {
+	// doesn't throw any errors
+	return x + y; // but doesn't give us a way to CALL it
+};
+
+//? Even with no arguements passed in, Parens are still needed
+const rollDie = () => {
+	return Math.floor(Math.random() * 6) + 1;
+};
+
+console.log(rollDie()); // prints random die roll
+
+//? UNLESS you only have one parameter! HA HA HA!!!
+//? Then you could GET RID of the Parens :O :O :O
+
+const square2 = (x) => {
+	return x ** 2; //? ** raises the power
+};
+
+console.log(square2(4)); //prints 16
+
+//* Implicit Returns
+//? When it's implied that you're returning something,
+//? so you actually LEAVE OUT the RETURN KEYWORD.
+//? This is indicated by REPLACING CURLY BRACKETS WITH PARENS
+//! THIS ONLY WORKS WITH ARROW FUNCTIONS!!!
+//! THIS ONLY WORKS WHEN YOUR RETURN EVALUATES TO A SINGLE EXPRESSION!!!
+
+const isEven = function (num) { // Regular function expression
+    return num % 2 === 0; 
+}
+
+const isEven2 = (num) => { // arrow functions with parens around param
+    return num % 2 === 0;
+}
+
+const isEven3 = num => { // arrow function without parens
+    return num % 2 === 0;
+}
+
+//! NOTICE THE PARENS REPLACING THE CURLY BRACKETS!!!
+const isEven4 = num => ( // arrow function with IMPLICIT RETURN
+    num % 2 === 0
+);
+
+const isEven5 = num => num % 2 === 0; // Written on a single line
+
+//! THESE ALL DO THE SAME THING
+
+const rollDie2 = () => ( // parens with no params
+    Math.floor(Math.random() * 6) + 1 // implicit return
+);
+
+console.log(rollDie2());
+
+//? Technically, you could even GET RID of IMPLICIT RETURN PARENS
+
+const add3 = a => a + a; // This works! lmao
+
+console.log(add3(3));
+
+//* Using Arrow functions with .map() and .forEach()
+
+const videogames = [
+    {
+        title: "Bioshock",
+        rating: 100,
+    },
+    {
+        title: "No Mans Sky",
+        rating: 100,
+    },
+    {
+        title: "Undertale",
+        rating: 100,
+    },
+    {
+        title: "Doki Doki Literature Club",
+        rating: 40,
+    }
+];
+
+//? Typical Arrow Function
+
+const videogameTitles = videogames.map((game) => {
+    return `${game.title} - ${game.rating / 10}/10`;
+})
+
+console.log(videogameTitles);
+
+//? Written with Implicit return on a single line (Kinda long...)
+
+const videogameTitles2 = videogames.map(game => `${game.title} - ${game.rating / 10}/10`);
+
+console.log(videogameTitles2);
+
+//? Written iwth implicit return on multiple lines
+
+const videogameTitles3 = videogames.map((game) => ( // Notice the Parens!!!
+    `${game.title} - ${game.rating / 10}/10`
+));
+
+console.log(videogameTitles3);
