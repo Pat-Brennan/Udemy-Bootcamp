0d6ad4a2af2318973b03b1d368508a32f7463a11
Patrick Brennan <patbrennan7@gmail.com>
2022-03-04 17:28:23 -0500

    Time Methods Started

---
+++ js-time-methods/app.js
@@ -0,0 +1,9 @@
+//* Time Methods!
+//? Methods that execute callback functions after a timer has run out
+
+//* setTimeout
+//? Takes in two arguements, a callback function, and an amount of time in milliseconds
+
+setTimeout(() => {
+    console.log("WOW I WAITED 3 SECONDS!")
+}, 3000);
