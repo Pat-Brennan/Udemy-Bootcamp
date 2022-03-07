521612f9ddfd4c035082d56a5af2a269752ba6fa
Patrick Brennan <patbrennan7@gmail.com>
2022-03-07 16:14:21 -0500

    setInterval Finished

--- js-time-methods/app.js
+++ js-time-methods/app.js
@@ -1,7 +1,7 @@
 //* Time Methods!
 //? Methods that execute callback functions after a timer has run out
 
-//* setTimeout
+//* setTimeout()
 //? Takes in two arguements, a callback function, and an amount of time in milliseconds
 
 console.log("Hello there Traveller!"); // This prints first
@@ -10,3 +10,20 @@ setTimeout(() => {
 }, 3000);
 
 console.log("May the wind be at your back!"); // This prints second
+
+//* setInterval()
+//? Repeatedly calls a function or executes a code snippet
+//? with a fixed time delay between each call.
+
+const id = setInterval(() => { // Same set up as setTimeout
+	console.log(Math.random());
+}, 1000); // Every second, this loop will start over and generate another random number
+
+//* clearInterval
+//? Used to break the loop created by setInterval().
+//? Global method that cancels a timed, or repeated action.
+
+// clearInterval(id);
+
+//! Worth noting, the pool of ID's used by setInterval and setTimeout are SHARED
+//! Meaning, clearInterval(), and clearTimeout() can be used INTERCHANGEABLY.
