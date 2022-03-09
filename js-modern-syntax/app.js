013e04b3d96e4315ca07f3283f8c03a46fe2a910
Patrick Brennan <patbrennan7@gmail.com>
2022-03-09 13:04:52 -0500

    Spread Syntax Continued

--- js-modern-syntax/app.js
+++ js-modern-syntax/app.js
@@ -28,3 +28,27 @@ function person2(name, greet = "Hey There", punc = "!") {
 } 
 console.log(person2("Jaoquin Phoenix")); // prints Hey there, Jaoquin Phoenix!
 console.log(person2("Bongo", "What up", "!?!?!"));
+
+//* Spread Syntax (...)
+//? Spread syntax allows an iterable such as an array to be EXPANDED
+//? in places where zero or more arguments(for function calls) or
+//? elements (for array literals) are expected, or an object expression
+//? to be expanded in places where zero or more key-value pairs (for object literals)
+//? are expected. Phew! That's a lot.
+
+//! The "Swiss army knife" of JS!
+
+//? This example is rigid, but works.
+function sum(x, y, z) {
+    return x + y + z;
+}
+
+const numbers = [1, 4, 666];
+
+console.log(sum(...numbers)); // prints 671
+console.log(sum([1, 4, 666])); // prints [1, 4, 666,], undefined, undefined because sum is expecting 2 more arguments
+console.log(sum(...[1, 4, 666])); // prints 671 becuase it SPREAD OUT the entries amongst the arguments
+console.log(sum(...[1, 4, 666, 420])); // still prints 671 because there aren't enought args for 420
+
+console.log(numbers); // prints array of numbers with brackets
+console.log(...numbers); // prints numbers... but takes brackets away and adds spaces
