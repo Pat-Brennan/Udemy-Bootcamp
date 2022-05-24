11a704bf9782779d670372b4d004951e3f79bf36
Patrick Brennan <patbrennan7@gmail.com>
2022-05-24 16:54:08 -0400

    Module Complete

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -2,4 +2,11 @@ const allLinks = document.querySelectorAll('a');
 
 for (let link of allLinks) {
     link.innerText = 'A LINK!'
+    link.style.color = 'red'
+    link.style.textDecorationColor = 'cyan'
+    link.style.textDecorationStyle = 'wavy'
 }
+
+// for (let link of allLinks) {
+//     link.style.color = 'cyan'
+// }
