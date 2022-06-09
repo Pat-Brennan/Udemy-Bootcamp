9ff69936a3a8a0e1b4aa30dd235e8a64253fc513
Patrick Brennan <patbrennan7@gmail.com>
2022-06-09 14:41:56 -0400

    Winning Score Select Updated

--- score-keeper-code-along/app.js
+++ score-keeper-code-along/app.js
@@ -2,11 +2,12 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-const reset = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto')
+const resetButton = document.querySelector('#reset');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = `${winningScoreSelect}`;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
@@ -29,10 +30,18 @@ p2Button.addEventListener('click', function () {
 	}
 });
 
-reset.addEventListener('click', function () {
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset);
+
+
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
 	p1Display.textContent = p1Score;
 	p2Display.textContent = p2Score;
-});
+};
