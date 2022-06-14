91a56c93bfcefb44e835521a9c864535d37ff5fa
Patrick Brennan <patbrennan7@gmail.com>
2022-06-14 16:56:26 -0400

    Bulma Installed

--- score-keeper-code-along/app.js
+++ score-keeper-code-along/app.js
@@ -2,7 +2,7 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-const winningScoreSelect = document.querySelector('#playto')
+const winningScoreSelect = document.querySelector('#playto');
 const resetButton = document.querySelector('#reset');
 
 let p1Score = 0;
@@ -31,17 +31,16 @@ p2Button.addEventListener('click', function () {
 });
 
 winningScoreSelect.addEventListener('change', function () {
-    winningScore = parseInt(this.value);
-    reset();
-})
+	winningScore = parseInt(this.value);
+	reset();
+});
 
 resetButton.addEventListener('click', reset);
 
-
 function reset() {
-    isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
+	isGameOver = false;
+	p1Score = 0;
+	p2Score = 0;
 	p1Display.textContent = p1Score;
 	p2Display.textContent = p2Score;
-};
+}
