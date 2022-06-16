1499e8d209ad5face62f887e4887fa2de4250565
Patrick Brennan <patbrennan7@gmail.com>
2022-06-16 16:53:40 -0400

    Callstack Briefly Covered

--- async-js-callstack/app.js
+++ async-js-callstack/app.js
@@ -4,3 +4,27 @@
 //? How JS "knows" what function is currently being run and what functions
 //? are called from within that function.
 
+//? A STACK is a "Last in - Last out" data structure in computer science.
+
+//* How it works
+//? When a script calls a function, the interpreter add it to the call stack
+//? and then starts carrying out the function.
+
+//? Any functions that are called by that function are added to the call stack
+//? further up, and run where their calls are reached.
+
+//? When the current function is finished, the interpreter takes it off the stack
+//? and resumes execution where it left off i nthe last code listing.
+
+//* Example
+
+//? Top of the stack of books
+const multiply = (a, b) => console.log(a * b); // Ooo this is multiply *** then go back down the stack ***
+
+//? We must go through this stack everytime we want to find an answer 
+const square = a => console.log(multiply(a, a)); // well what's multiply()?
+
+//? Think of this as the bottom of a stack of books
+const isRightTriangle = (a, b, c) => {
+  return square(a) + square(b) === square(c); // What's square(a)?
+};
