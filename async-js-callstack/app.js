dc1747770b9f05e8cd2f6f1a68d13449ba3f7e86
Patrick Brennan <patbrennan7@gmail.com>
2022-06-16 16:21:32 -0400

    Call Stack Started

---
+++ async-js-callstack/app.js
@@ -0,0 +1,6 @@
+//* The Call Stack!
+//? The Mechanism the JS interpreter uses to keep track of its place in a script
+//? that calls multiple functions.
+//? How JS "knows" what function is currently being run and what functions
+//? are called from within that function.
+
