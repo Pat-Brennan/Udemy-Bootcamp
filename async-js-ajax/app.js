bc08ab7fad2bbfafaebc0258ee2c77b070db8510
Patrick Brennan <patbrennan7@gmail.com>
2022-06-28 12:19:45 -0400

    AJAX Started

---
+++ async-js-ajax/app.js
@@ -0,0 +1,10 @@
+//* AJAX
+//? Asynchronus JavaScript and XML
+//? A means to update the webpage without needing to reload
+//? Example: Scrolling through reddit, hittin the bottom, and new posts are loaded.
+
+//* API
+//? Application Programming Interface
+//? Broad term for any computer to communicate with another piece of software.
+
+
