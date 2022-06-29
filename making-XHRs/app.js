4722b5068d28e25b4d20fe9dcc9bb1782608736f
Patrick Brennan <patbrennan7@gmail.com>
2022-06-29 16:05:21 -0400

    Code Cleaned Up

--- making-XHRs/app.js
+++ making-XHRs/app.js
@@ -6,7 +6,6 @@
 
 //!  Not super important to remember how to do this!
 
-
 //*The Process
 
 //? First, create a new XMLhttpRequest
@@ -15,25 +14,24 @@ const req = new XMLHttpRequest();
 
 // callback for onload
 req.onload = function () {
-  console.log("WE SUCCEEDED!"); // show the request works
-  data = JSON.parse(this.responseText); // convert the acuired JSON to JS Object
-  console.log(data); // then print what you need
-  console.log(data.name, data.height, data.hair_color);
-  console.log(data.name[3]);
-}
+	console.log('WE SUCCEEDED!'); // show the request works
+	data = JSON.parse(this.responseText); // convert the acuired JSON to JS Object
+	console.log(data); // then print what you need
+	console.log(data.name, data.height, data.hair_color);
+	console.log(data.name[3]);
+};
 // callback for errors
 req.onerror = function () {
-  console.log("WE DID NOT SUCCEED!");
-  console.log(this);
-}
+	console.log('WE DID NOT SUCCEED!');
+	console.log(this);
+};
 
 //? Second, Open that request
 //? 1st arg = type of request, 2nd arg = where request is being made
-req.open("GET", "https://swapi.dev/api/people/1/") 
+req.open('GET', 'https://swapi.dev/api/people/1/');
 
 //? Third, send it!
-req.send(); 
-
+req.send();
 
 //! All of this... for one request!
 
