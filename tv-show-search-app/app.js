0df5325c5640a2fe20b1243e9b3d2f252f4e5b0b
Patrick Brennan <patbrennan7@gmail.com>
2022-07-01 14:18:35 -0400

    Code Cleaned Up

--- tv-show-search-app/app.js
+++ tv-show-search-app/app.js
@@ -5,7 +5,7 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async (e) => {
 	e.preventDefault();
 	const searchTerm = form.elements.query.value;
-	const config = { params: { q: searchTerm} };
+	const config = { params: { q: searchTerm } };
 	const res = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
 	makeImages(res.data);
 	form.elements.query.value = '';
