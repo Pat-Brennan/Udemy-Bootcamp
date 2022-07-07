045a306cec43f40347a0031aae2e6c7841e51bf1
Patrick Brennan <patbrennan7@gmail.com>
2022-07-06 20:41:36 -0400

    Code Cleaned Up

--- OOP-intro/app.js
+++ OOP-intro/app.js
@@ -4,26 +4,27 @@
 
 //? Converts RGB to HEX
 function hex(r, g, b) {
-  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
 hex(255, 100, 25);
 // "#ff6419"
 
 function makeColor(r, g, b) {
-  const color = {}; //? Create the object {}
-  color.r = r; //? Add some properties
-  color.g = g;
-  color.b = b;
-  color.rgb = () => { //? Add some methods
-    const { r, g, b } = this;
-    return `rgb(${r}, ${g}, ${b})`;
-  };
-  color.hex = () => {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  }
-  return color; //? Then return the object
+	const color = {}; //? Create the object {}
+	color.r = r; //? Add some properties
+	color.g = g;
+	color.b = b;
+	color.rgb = () => {
+		//? Add some methods
+		const { r, g, b } = this;
+		return `rgb(${r}, ${g}, ${b})`;
+	};
+	color.hex = () => {
+		const { r, g, b } = this;
+		return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	};
+	return color; //? Then return the object
 }
 
 const firstColor = makeColor(35, 255, 255);
